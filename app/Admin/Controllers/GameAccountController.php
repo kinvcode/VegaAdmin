3d419005cd05a0be4444897fc9601d9c99d9eb06
kinvcode <kinvcode@gmail.com>
2024-03-22 18:01:44 +0800

    游戏账号不显示角色创建时间； 游戏账号显示方案;

--- app/Admin/Controllers/GameAccountController.php
+++ app/Admin/Controllers/GameAccountController.php
@@ -12,6 +12,18 @@ use Illuminate\Support\Facades\DB;
 
 class GameAccountController extends AdminController
 {
+    // 方案说明
+    // 方案1：隔一天玩一次，比如1号玩，然后3号再玩
+    // 方案2：隔两天玩一次，比如1号玩，然后4号再玩
+    // 方案3：固定星期三玩一次
+    // 方案4：每天都玩
+    public $plans = [
+        1 => '方案1',
+        2 => '方案2',
+        3 => '方案3',
+        4 => '方案4'
+    ];
+
     /**
      * Make a grid builder.
      *
@@ -19,23 +31,27 @@ class GameAccountController extends AdminController
      */
     protected function grid()
     {
-        return Grid::make(new GameAccount(['computer','vpn']), function (Grid $grid) {
+        $controller = $this;
+        return Grid::make(new GameAccount(['computer', 'vpn']), function (Grid $grid) use($controller){
 //            $grid->column('id')->sortable();
-            $grid->column('computer.pc_name','所属机器');
-            $grid->column('vpn.remark','所属IP');
+            $grid->column('computer.pc_name', '所属机器');
+            $grid->column('vpn.remark', '所属IP');
             $grid->column('email');
             $grid->column('email_password');
             $grid->column('account');
             $grid->column('password');
-            $grid->column('game_status');
-            $grid->column('user_nums');
-            $grid->column('bind_email');
+            $grid->column('plan')->display(function($plan) use($controller){
+                return $controller->plans[$plan];
+            });
+//            $grid->column('game_status');
+//            $grid->column('user_nums');
+//            $grid->column('bind_email');
             $grid->column('account_created');
-            $grid->column('user_created');
-            $grid->column('restriction_times');
-            $grid->column('restriction_time');
-            $grid->column('blocked_times');
-            $grid->column('blocked_time');
+//            $grid->column('user_created');
+//            $grid->column('restriction_times');
+//            $grid->column('restriction_time');
+//            $grid->column('blocked_times');
+//            $grid->column('blocked_time');
 //            $grid->column('owner_ip');
 //            $grid->column('owner_pc');
 
@@ -55,10 +71,10 @@ class GameAccountController extends AdminController
      */
     protected function detail($id)
     {
-        return Show::make($id, new GameAccount(['computer','vpn']), function (Show $show) {
+        return Show::make($id, new GameAccount(['computer', 'vpn']), function (Show $show) {
 //            $show->field('id');
-            $show->field('computer.pc_name','所属机器');
-            $show->field('vpn.remark','所属IP');
+            $show->field('computer.pc_name', '所属机器');
+            $show->field('vpn.remark', '所属IP');
             $show->field('email');
             $show->field('email_password');
             $show->field('account');
@@ -67,7 +83,7 @@ class GameAccountController extends AdminController
             $show->field('user_nums');
             $show->field('bind_email');
             $show->field('account_created');
-            $show->field('user_created');
+//            $show->field('user_created');
             $show->field('restriction_times');
             $show->field('restriction_time');
             $show->field('blocked_times');
@@ -83,21 +99,22 @@ class GameAccountController extends AdminController
      */
     protected function form()
     {
-        return Form::make(new GameAccount(), function (Form $form) {
+        $controller = $this;
+        return Form::make(new GameAccount(), function (Form $form) use($controller){
             $form->display('id');
-            $form->select('pc_id')->options('/api/computers')->load('ip_id','/api/vpns');
+            $form->select('pc_id')->options('/api/computers')->load('ip_id', '/api/vpns');
             $form->select('ip_id');
             $form->email('email');
             $form->text('email_password');
             $form->text('account');
             $form->text('password');
-            if($form->isEditing())
-            {
+            $form->select('plan')->options($controller->plans);
+            if ($form->isEditing()) {
                 $form->text('game_status');
                 $form->text('user_nums');
                 $form->text('bind_email');
                 $form->datetime('account_created');
-                $form->datetime('user_created');
+//                $form->datetime('user_created');
                 $form->text('restriction_times');
                 $form->datetime('restriction_time');
                 $form->text('blocked_times');
@@ -110,14 +127,13 @@ class GameAccountController extends AdminController
     {
         $ip_id = $request->get('q');
 
-        $data = DB::table('game_account')->where('ip_id',$ip_id)->get();
+        $data = DB::table('game_account')->where('ip_id', $ip_id)->get();
 
         $response = [];
-        foreach ($data as $item)
-        {
+        foreach ($data as $item) {
             $response[] = [
                 'id' => $item->id,
-                'text'=>$item->email
+                'text' => $item->email
             ];
         }
 
